d6d8e6b0e4cdc05b027a651167027b4b01d93cdb
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-11-05 20:57:05 +0300

    initialCommit

---
+++ element-search/popups/task.js
@@ -0,0 +1,17 @@
+const modalMain = document.getElementById(`modal_main`);
+const modalClose = Array.from(document.getElementsByClassName('modal__close'));
+const modalSuccess = document.getElementById('modal_success');
+const onBtn = document.querySelector('a.btn_danger')
+
+modalMain.classList.add(`modal_active`);
+
+for (let i = 0; i < modalClose.length; i++) {
+  modalClose[i].onclick = function () {
+    this.closest('.modal_active').classList.remove('modal_active');
+  }
+}
+
+onBtn.onclick = function () {
+  modalMain.classList.remove(`modal_active`);
+  modalSuccess.classList.add(`modal_active`);
+}
