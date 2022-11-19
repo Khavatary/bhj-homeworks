c3a1d7bf22bf957fe54d8a40c10cda2fc6a97fdd
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-11-19 18:34:20 +0300

    initial commit

---
+++ dom/book-reader/task.js
@@ -0,0 +1,20 @@
+const arrFont = Array.from(document.querySelectorAll(`.font-size`));
+const book = document.getElementById(`book`);
+
+for (let i = 0; i < arrFont.length; i++) {
+  arrFont[i].onclick = function () {
+    if (arrFont[i].classList.contains(`font-size_small`)) {
+      book.classList.add(`book_fs-small`);
+      book.classList.remove(`book_fs-big`);
+    } else if (arrFont[i].classList.contains(`font-size_big`)) {
+      book.classList.add(`book_fs-big`);
+      book.classList.remove(`book_fs-small`);
+    } else {
+      book.classList.remove(`book_fs-big`);
+      book.classList.remove(`book_fs-small`);
+    }
+    book.querySelector(`.font-size_active`).classList.remove(`font-size_active`);
+    arrFont[i].classList.add(`font-size_active`);
+    return false;
+  }
+}
