b1712073251c44c469c315ea3d950cd2c880ef7a
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-11-28 18:02:16 +0300

    initial commit

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,26 @@
+const items = document.getElementById(`items`);
+const loader = document.getElementById(`loader`);
+
+let xhr = new XMLHttpRequest();
+xhr.open(`GET`, `https://netology-slow-rest.herokuapp.com`);
+xhr.send();
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === xhr.DONE) {
+    loader.classList.remove(`loader_active`);
+    let valute = JSON.parse(xhr.response).response.Valute;
+    for (let value in valute) {
+      items.insertAdjacentHTML(`beforeEnd`, `
+       <div class="item">
+         <div class="item__code">
+         ${valute[value].CharCode}
+         </div>
+         <div class="item__value">
+         ${valute[value].Value}
+         </div>
+         <div class="item__currency">
+         руб.
+         </div>
+      </div>`);
+    }
+  };
+};
