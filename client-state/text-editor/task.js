2eb084fc486f0b26669799e325e4dd20042ef515
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-12-07 20:00:27 +0300

    edit chek key

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,14 @@
 const editor = document.getElementById(`editor`);
 
 editor.oninput = function () {
+
   localStorage.text = editor.value;
 }
 
-document.addEventListener(`DOMContentLoaded`, () =>{
-  editor.value = localStorage.text;
-})  
+document.addEventListener(`DOMContentLoaded`, () => {
+  if (localStorage.text === undefined) {
+    editor.value = ``;
+  } else {
+    editor.value = localStorage.text;
+  }
+})
